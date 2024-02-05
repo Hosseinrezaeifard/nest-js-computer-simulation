e2a70f892da3d4ac8dd3eb4bd45765181fe3abba
Hossein Rezaei Fard <57575843+Hosseinrezaeifard@users.noreply.github.com>
2024-02-05 03:00:14 -0800

    Fix a bug

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -7,7 +7,7 @@ export class CpuService {
 
   compute(a: number, b: number) {
     console.log('Drawing 10 watts of power from Power Service');
-    this.powerService(10);
+    this.powerService.supplyPower(10);
     return a + b;
   }
 }
